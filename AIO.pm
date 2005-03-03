29e1b56195c984004b7e9de07654a1e9aadb1c4a
Marc Lehmann <schmorp@schmorp.de>
2005-03-03 17:53:57 +0000

    Import of MLEHMANN/Linux-AIO-1.41 from CPAN.
    gitpan-cpan-distribution: Linux-AIO
    gitpan-cpan-version:      1.41
    gitpan-cpan-path:         MLEHMANN/Linux-AIO-1.41.tar.gz
    gitpan-cpan-author:       MLEHMANN
    gitpan-cpan-maturity:     released

--- AIO.pm
+++ AIO.pm
@@ -34,7 +34,7 @@ package Linux::AIO;
 use base 'Exporter';
 
 BEGIN {
-   $VERSION = 1.4;
+   $VERSION = 1.41;
 
    @EXPORT = qw(aio_read aio_write aio_open aio_close aio_stat aio_lstat aio_unlink);
    @EXPORT_OK = qw(poll_fileno poll_cb min_parallel max_parallel nreqs);
@@ -136,7 +136,7 @@ L<Coro>.
 
 =head1 AUTHOR
 
- Marc Lehmann <pcg@goof.com>
+ Marc Lehmann <schmorp@schmorp.de>
  http://home.schmorp.de/
 
 =cut
